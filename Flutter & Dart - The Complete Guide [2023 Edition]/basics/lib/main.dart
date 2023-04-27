d6993ba0059a5e0a63b9ef3d965759168319265d
chidambararaja.b <chidambararaja.b@accubits.com>
2023-04-27 09:26:20 +0530

    [Basics]: Gradient BG

--- Flutter & Dart - The Complete Guide [2023 Edition]/basics/lib/main.dart
+++ Flutter & Dart - The Complete Guide [2023 Edition]/basics/lib/main.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Text('Flutter'),
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.deepOrange,
+              Colors.deepOrangeAccent,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: const Center(
+          child: Text(
+            'Flutter',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    ),
   ));
 }
