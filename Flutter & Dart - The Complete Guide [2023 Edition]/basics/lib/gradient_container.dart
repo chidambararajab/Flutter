08b4be38eff26ad77b33c3a5517dcdeb90f29aa9
chidambararaja.b <chidambararaja.b@accubits.com>
2023-04-27 19:43:27 +0530

    [Basics]: Widgets with multiple constructor

--- Flutter & Dart - The Complete Guide [2023 Edition]/basics/lib/gradient_container.dart
+++ Flutter & Dart - The Complete Guide [2023 Edition]/basics/lib/gradient_container.dart
@@ -4,6 +4,16 @@ import 'package:basics/styled_text.dart';
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key, required this.colors});
 
+  /**
+   * Here the point is we can add multiple constructors and make the widgets more customizable.
+   * We can call like GradientContainer.orange() as a widget as well to show the orange as a colors.
+   */ ///
+  // GradientContainer.orange({super.key})
+  //     : colors = [
+  //         Colors.deepOrange,
+  //         Colors.deepOrangeAccent,
+  //       ];
+
   final List<Color> colors;
 
   @override
