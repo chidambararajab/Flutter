2101f3209260682b8c273c6575fec1a1f4f7eef4
chidambararaja.b <chidambararaja.b@accubits.com>
2023-04-28 10:42:20 +0530

    [Adv_Basics]: Rechanged components

--- Flutter & Dart - The Complete Guide [2023 Edition]/adv_basics/lib/quiz.dart
+++ Flutter & Dart - The Complete Guide [2023 Edition]/adv_basics/lib/quiz.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:adv_basics/gradient_container.dart';
 import 'package:adv_basics/start_screen.dart';
 
-class Quiz extends StatelessWidget {
+class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
