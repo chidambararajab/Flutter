e1ad644abec01504e545ea4333efeba526fdbad9
chidambararaja.b <chidambararaja.b@accubits.com>
2023-05-04 11:32:10 +0530

    [expense_tracker]: To support fpr only portrate mode / and restrict landscape mode

--- Flutter & Dart - The Complete Guide [2023 Edition]/expense_tracker/lib/main.dart
+++ Flutter & Dart - The Complete Guide [2023 Edition]/expense_tracker/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
