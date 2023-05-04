87cc1cfa9fa3cb6343011cb8061c0c8a0cf6bb40
chidambararaja.b <chidambararaja.b@accubits.com>
2023-05-04 18:06:13 +0530

    [meals]: category_grid_item added

--- Flutter & Dart - The Complete Guide [2023 Edition]/meals/lib/screens/categories_screen.dart
+++ Flutter & Dart - The Complete Guide [2023 Edition]/meals/lib/screens/categories_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -11,33 +13,20 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Categories'),
       ),
       body: GridView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 10,
+        ),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: const [
-          Text(
-            '1',
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            '2',
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            '3',
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            '4',
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            '5',
-            style: TextStyle(color: Colors.white),
-          ),
+        children: [
+          ...availableCategories
+              .map((e) => CategoryGridItem(category: e))
+              .toList()
         ],
       ),
     );
