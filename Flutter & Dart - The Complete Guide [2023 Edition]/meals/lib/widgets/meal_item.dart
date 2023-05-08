ccf2a836ac336fee6fd4952707b4ee1f7b63f751
chidambararaja.b <chidambararaja.b@accubits.com>
2023-05-08 13:25:51 +0530

    [meals]: meal_item_trai

--- Flutter & Dart - The Complete Guide [2023 Edition]/meals/lib/widgets/meal_item.dart
+++ Flutter & Dart - The Complete Guide [2023 Edition]/meals/lib/widgets/meal_item.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item_trai.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal});
 
   final Meal meal;
+
+  String get getComplexity {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
+  String get getAffordability {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,7 +68,27 @@ class MealItem extends StatelessWidget {
                       height: 10,
                     ),
                     Row(
-                      children: [],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealItemTrait(
+                          icon: Icons.lock_clock,
+                          label: '${meal.duration} min',
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        MealItemTrait(
+                          icon: Icons.work,
+                          label: getComplexity,
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        MealItemTrait(
+                          icon: Icons.attach_money,
+                          label: getAffordability,
+                        ),
+                      ],
                     ),
                   ],
                 ),
