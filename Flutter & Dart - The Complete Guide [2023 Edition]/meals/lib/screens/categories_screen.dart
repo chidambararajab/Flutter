a299cc21ef45fbb0a8e44c8a6175d29fdb54c166
chidambararaja.b <chidambararaja.b@accubits.com>
2023-05-08 13:14:45 +0530

    [meals]: rename

--- Flutter & Dart - The Complete Guide [2023 Edition]/meals/lib/screens/categories_screen.dart
+++ Flutter & Dart - The Complete Guide [2023 Edition]/meals/lib/screens/categories_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/category.dart';
-import 'package:meals/screens/meals.dart';
+import 'package:meals/screens/meals_screen.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
